9e8fcd9ac425f324b706caa9028dc4b49ec5c672
Yitao Li <yitao@rstudio.com>
2020-05-26 14:59:48 +0000

    enable arrow
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- sparklyr_sdf_import_benchmark/sparklyr_sdf_import_benchmark.R
+++ sparklyr_sdf_import_benchmark/sparklyr_sdf_import_benchmark.R
@@ -2,10 +2,17 @@ library(sparklyr)
 
 spark_master <- Sys.getenv("SPARK_MASTER")
 spark_home <- Sys.getenv("SPARK_HOME")
+enable_arrow <- Sys.getenv("ENABLE_ARROW")
 
 df <- data.frame(lapply(seq(10), function(c) sapply(runif(n = 100000, min = -2147483648, max = 2147483647), as.integer)))
 colnames(df) <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")
 
-sc <- spark_connect(master = spark_master, spark_home = spark_home)
+config <- spark_config()
+if (identical(enable_arrow, "TRUE")) {
+  config$`sparklyr.arrow` <- TRUE
+  invisible(assertthat::assert_that(require("arrow")))
+}
+
+sc <- spark_connect(master = spark_master, spark_home = spark_home, config = config)
 
 cat(system.time(sdf_copy_to(sc, df, overwrite = TRUE))[[3]], "\n")
