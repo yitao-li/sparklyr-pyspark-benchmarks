e8d6ace30b6b4a1a1a4acdb3fe7d8c8a434b2977
Yitao Li <yitao@rstudio.com>
2020-05-26 13:15:11 +0000

    script to benchmark importing local dataframe with pyspark
    Signed-off-by: Yitao Li <yitao@rstudio.com>

---
+++ pyspark_sdf_copy_benchmark/pyspark_sdf_copy_benchmark.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import timeit
+from pyspark.sql import SparkSession
+
+spark = SparkSession\
+        .builder\
+        .appName("pyspark_benchmark")\
+        .getOrCreate()
+
+df = pd.DataFrame(np.random.randint(-2147483648,2147483647,size=(1000000, 10)), columns=list('ABCDEFGHIJ'))
+
+def run():
+    global df
+    sdf = spark.createDataFrame(df)
+    return sdf
+
+print(timeit.timeit(run, number = 1))
