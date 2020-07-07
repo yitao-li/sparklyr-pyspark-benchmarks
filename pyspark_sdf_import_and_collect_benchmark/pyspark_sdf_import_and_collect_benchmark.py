9c40f18220f429be702ce0d2d77f2f03d6f22b3e
Yitao Li <yitao@rstudio.com>
2020-07-07 15:03:36 +0000

    avoid `global`
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- pyspark_sdf_import_and_collect_benchmark/pyspark_sdf_import_and_collect_benchmark.py
+++ pyspark_sdf_import_and_collect_benchmark/pyspark_sdf_import_and_collect_benchmark.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
-import timeit
+import functools
 import os
+import timeit
 from pyspark.sql import SparkSession
 
 spark = SparkSession\
@@ -9,11 +10,11 @@ spark = SparkSession\
         .appName("pyspark_benchmark")\
         .getOrCreate()
 
-def run():
-    global df
-    sdf = spark.createDataFrame(df)
-    sdf.collect()
+def run(df):
+  sdf = spark.createDataFrame(df)
+  sdf.collect()
 
 for _ in range(int(os.getenv('NUM_ITERS'))):
   df = pd.DataFrame(np.random.randint(-2147483648, 2147483647,size=(100000, 10)), columns=list('ABCDEFGHIJ'))
-  print(timeit.timeit(run, number = 1))
+  t = timeit.Timer(functools.partial(run, df))
+  print(t.timeit(number = 1))
