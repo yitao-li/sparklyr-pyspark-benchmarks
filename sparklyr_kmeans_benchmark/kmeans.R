cdbb653ba1370f2398df8e9488b7ccc3b2c80710
Yitao Li <yitao@rstudio.com>
2020-07-13 14:10:39 +0000

    make sparklyr and pyspark kmeans parameters consistent
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- sparklyr_kmeans_benchmark/kmeans.R
+++ sparklyr_kmeans_benchmark/kmeans.R
@@ -1,10 +1,11 @@
 library(sparklyr)
 
 sc <- spark_connect(master = "yarn-client", spark_home = "/usr/lib/spark", scala_version = "2.12")
-sdf <- spark_read_parquet(sc, path = "hdfs:///user/spark/warehouse/kmeans-data.parquet")
 
-cat(system.time({
-res <- ml_kmeans(sdf, ~., k = 3L, max_iter = 10000, tol = 1e-8)
-})[[3]], "\n")
+for (x in seq(10))
+  cat(system.time({
+    sdf <- spark_read_parquet(sc, path = "hdfs:///user/spark/warehouse/kmeans-data.parquet")
+    res <- ml_kmeans(sdf, ~., k = 3L, seed = 1L)
+  })[[3]], "\n")
 
 # print(res)
